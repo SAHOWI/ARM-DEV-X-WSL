7b23142dbf4dfb742c994c58ba458690960c3229
SAHOWI <wienecke.ch@gmail.com>
2021-09-29 14:37:55 +0200

    [A] added initial version of hello_world.asm to check toolchain

---
+++ src/hello_world.asm
@@ -0,0 +1,16 @@
+# ARM Assembly Examples
+# Filename: hello_world.asm
+# Version:  1.0
+# Purpos:   Validate Toolchain
+
+
+.global		_start
+.section	.text
+
+_start:
+	mov r15, #64      # load Value 64 to register R15
+
+
+
+.section	.data
+
